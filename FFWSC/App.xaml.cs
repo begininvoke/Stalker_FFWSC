84453f2cac4d52746007230ca6dcc19cb51b9870
taskdelay <zapezhman@gmail.com>
2020-02-20 15:45:38 +0330

    update

---
+++ FFWSC/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FFWSC
+{
+    /// <summary>
+    /// github page https://github.com/zapezhman
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ FFWSC_Core/status.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFWSC_Core
+{
+    /// <summary>
+    /// github page https://github.com/zapezhman
+    /// </summary>
+    public class Status
+    {
+        public string File { get; set; }
+        //public string Status { get; set; }
+
+        public string Color { get; set; }
+
+
+    }
+
+}
